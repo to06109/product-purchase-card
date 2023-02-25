7be80cbc4279c7c2348f869ec8fdce0fd4f82b0f
Yesong Choi <to06109@naver.com>
2023-02-26 03:19:44 +0900

    design. theme 토큰 생성

---
+++ src/theme/tokens.js
@@ -0,0 +1,30 @@
+export const tokens = {
+  colors: {
+    'color-black': '#000000',
+    'color-white': '#ffffff',
+    'color-secondary': '#6b90dc',
+    'color-gray': {
+      '100': '#f1f2f4',
+      '200': '#dee0e3',
+      '300': '#999999',
+      '400': '#333333',
+    },
+    'color-primary': {
+      ' 100': '#03c75a',
+    },
+    'color-primary': {
+      '200': '#0aae4f',
+    },
+  },
+  fontSize: {
+    base: '1.125rem',
+  },
+  fontFamily: {
+    'spoqa-han-sans-neo': 'Spoqa Han Sans Neo',
+  },
+  borderRadius: {
+    none: '0',
+    xs: '0.3125rem',
+    sm: '0.375rem',
+  },
+}
